a6d8ee5257873e060aaf75b278d786b3b1b6b5ea
Aaron Lisker <aelisker@gmail.com>
2020-03-25 22:31:17 -0700

    absolute starter

--- script.js
+++ script.js
@@ -2,16 +2,63 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var lowerConfirm = function() {
+  var lower = window.confirm("Would you like lower case characters?");
+  return lower;
+};
+var upperConfirm = function() {
+  var upper = window.confirm("Would you like upper case characters?");
+  return upper;
+};
+var numberConfirm = function() {
+  var number = window.confirm("Would you like numbers?");
+  return number;
+};
+var specialConfirm = function() {
+  var special = window.confirm("Would you like special characters?");
+  return special;
+};
 
-  passwordText.value = password;
-
+var passwordInfo = {
+  lower: lowerConfirm(),
+  upper: upperConfirm(),
+  number: numberConfirm(),
+  special: specialConfirm(),
 }
 
+if (passwordInfo.lower == false && passwordInfo.upper == false && passwordInfo.number == false && passwordInfo.special == false) {
+  alert("You need at least one type of character!");
+  lowerConfirm();
+}
+
+
+
+
+
+
+
+
+var getCritera = function() {
+ 
+}
+
+// Write password to the #password input
+// function writePassword() {
+//   // var password = generatePassword();
+//   // var passwordText = document.querySelector("#password");  
+
+//   // passwordText.value = password;
+//   window.alert(lowerConfirm);
+
+// }
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", getCritera);
+
+//Print to HTML with this
+// var test = "dddd";
+// document.getElementById("password").innerHTML = test;
