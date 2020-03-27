f095ad9dd5c7d1050d9ba76ebabf0b02925f983f
Aaron Lisker <aelisker@gmail.com>
2020-03-26 21:55:32 -0700

    working, need ability to keep using without refresh

--- script.js
+++ script.js
@@ -96,19 +96,8 @@ function writePassword(result) {
   passwordText.value = password;
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", getLength);
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");  
-
-//   passwordText.value = password;
-// }
-
-
 
 //Print to HTML with this
 // var test = "dddd";
