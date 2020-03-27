6b6eb860e437a89668e401b7cc32eb02cb9f6ba5
Aaron Lisker <aelisker@gmail.com>
2020-03-26 18:23:57 -0700

    added storage array for potential characters based on criteria prompts

--- script.js
+++ script.js
@@ -4,21 +4,32 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//stores criteria for password generation
 var criteria = [];
 
+//stores potential characters based on criteria
+var potentialChar = [];
+
+//all potential characters
+var lowerCharArray = ['abcdefghijklmnopqrstuvwxyz'];
+var upperCharArray = ['ABCDEFGHIJKLMNOPQRSTUVWZYZ'];
+var numberCharArray = ['0123456789'];
+var specialCharArray = ['!\"#$%&()*+`,\'-./:;<=>?@[]^_{}|~'];
+
+//gets length of password
 var getLength = function() {
-  var passwordLength = window.prompt("Choose a length between eight and 128 characters.");
-  var passwordLength = parseInt(passwordLength);
-  if (passwordLength < 8 || passwordLength > 128) {
+  var passwordLengthPrompt = window.prompt("Choose a length between eight and 128 characters.");
+  var passwordLengthPrompt = parseInt(passwordLengthPrompt);
+  if (passwordLengthPrompt < 8 || passwordLengthPrompt > 128) {
     window.alert("Your password must be between eight and 128 characters. Please try again.");
     return getLength();
   }
-  if (isNaN(passwordLength)) {
+  if (isNaN(passwordLengthPrompt)) {
     window.alert("Your password must be a number. Please try again.");
     return getLength();
   }
   //store length in array
-  criteria.push(['length',passwordLength]);
+  criteria.passwordLength = passwordLengthPrompt;
 
   //take down criteria
   getCriteria();
@@ -34,14 +45,37 @@ var getCriteria = function() {
     return getCriteria();
   }
   //log booleans to array
-  criteria.push(['lower',lower]);
-  criteria.push(['upper',upper]);
-  criteria.push(['number',number]);
-  criteria.push(['special',special]);
+  criteria.lower = lower;
+  criteria.upper = upper;
+  criteria.number = number;
+  criteria.special = special;
   
   //has this logged?
-  alert(criteria);
+  console.log(criteria);
+
+  //call function to create array with potential characters
+  generatePotentialCharacters();
 }
+
+var generatePotentialCharacters = function() {
+  //push character sets to potential character array
+  if (criteria.lower) {
+    potentialChar.push(lowerCharArray);
+  }
+  if (criteria.upper) {
+    potentialChar.push(upperCharArray);
+  }
+  if (criteria.number) {
+    potentialChar.push(numberCharArray)
+  }
+  if (criteria.special) {
+    potentialChar.push(specialCharArray)
+  }
+
+  //join all potential characters together in array
+  potentialChar = potentialChar.join('');
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", getLength);
